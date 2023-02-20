b9faf81fddb9524de9b00f4964e8e5ef9931f66d
Vitor Zillig <vitorzillig.azapfy@gmail.com>
2023-02-20 16:40:36 -0300

    API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState, ClassAttributes } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
+
+interface InputRefProps {
+  value:string
+}
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [data, setData] = useState({})
+
+  const inputRef = useRef<InputRefProps>(null)
+
+  const getFood = async(value:string | undefined)=>{
+    const response = await axios.post("https://trackapi.nutritionix.com/v2/natural/nutrients", {
+      "query": value,
+      "timezone": "America/Sao_Paulo",
+      "line_delimited": false,
+      "use_raw_foods": false,
+      "use_branded_foods": false
+    }, {
+      headers:{
+        "x-app-id":"8d0d9ff9",
+        "x-app-key":"1a77707f133f3e3c25f41c8e9673bd1a",
+        "x-remote-user-id":"0",
+      }
+    })
+    setData(response)
+    console.log(data)
+  }
 
   return (
     <div className="App">
@@ -15,17 +42,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Alimentos</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input ref={inputRef}/>
+        <button onClick={() =>getFood(inputRef.current?.value) }>
+          Pesquisar
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       {JSON.stringify(data)}
       </p>
     </div>
   )
